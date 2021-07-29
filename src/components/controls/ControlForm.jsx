0580c2a631743b912e2d0f132c7b4907979e06bf
Casey Cameron <mcasey.cameron@gmail.com>
2021-07-29 15:46:46 -0700

    Method hookup attempt

--- src/components/controls/ControlForm.jsx
+++ src/components/controls/ControlForm.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 
-const ControlForm = () => {
+const ControlForm = (handleMethod) => {
   return (
     <form>
-      <label hfmlFor='url'>URL:</label>
+      <label>URL:</label>
       <input
         type='text'
         placeholder='URL'
       />
       <button>Go!</button>
       <br />
-      <button value='GET'>GET</button>
-      <button value='POST'>POST</button>
-      <button value='PUT'>PUT</button>
-      <button value='PATCH'>Patch</button>
-      <button value='DELETE'>DELETE</button>
+      <button value='GET' onClick={handleMethod}>GET</button>
+      <button value='POST' onClick={handleMethod}>POST</button>
+      <button value='PUT' onClick={handleMethod}>PUT</button>
+      <button value='PATCH' onClick={handleMethod}>Patch</button>
+      <button value='DELETE' onClick={handleMethod}>DELETE</button>
       <br />
       <textarea type='text' placeholder='Raw JSON Body' />
     </form>
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -3,13 +3,21 @@ import ControlForm from '../components/controls/ControlForm';
 import Response from '../components/controls/Response';
 
 export default class Home extends Component {
+  state = {
+    method: 'GET'
+  }
+
+  handleMethod = ({ target }) => {
+    console.log({ target });
+  };
+
   render() {
     return (
       <>
         <header className="test">
           <h1>RESTless</h1>
         </header>
-        <ControlForm />
+        <ControlForm handleMethod={this.handleMethod} />
         <Response />
       </>
     );
