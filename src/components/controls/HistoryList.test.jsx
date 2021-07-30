1169f3b8cdbe4b410c62f920d5f5b448a92dd1f3
Casey Cameron <mcasey.cameron@gmail.com>
2021-07-30 14:54:22 -0700

    HistoryList better passing test

--- src/components/controls/HistoryList.test.jsx
+++ src/components/controls/HistoryList.test.jsx
@@ -3,15 +3,24 @@ import { render, cleanup } from '@testing-library/react';
 import HistoryList from './HistoryList';
 
 describe('Method History Container', () => {
+  const histories = [
+    {
+      method: 'GET',
+      urlValue: 'https://last-airbender-api.herokuapp.com/api/v1/characters/5cf5679a915ecad153ab68c8',
+    },
+    {
+      method: 'POST',
+      urlValue: 'https://last-airbender-api.herokuapp.com/api/v1/characters/5cf5679a915ecad153ab68c8',
+    }
+  ];
+
   afterEach(() => cleanup());
+
   it('Displays a list of method history items', () => {
-    const histories = [
-      {
-        method: 'GET',
-        urlValue: 'https://last-airbender-api.herokuapp.com/api/v1/characters/5cf5679a915ecad153ab68c8'
-      }
-    ]
-    const { getByText } = render(<HistoryList history={histories} />);
-    expect(getByText('GET')).toBeDefined();
+
+    const { asFragment } = render(<HistoryList history={histories} />);
+    expect(asFragment()).toMatchSnapshot();
+    // const { getByText } = render(<HistoryList history={histories} />);
+    // expect(getByText('GET')).toBeDefined();
   });
 });
