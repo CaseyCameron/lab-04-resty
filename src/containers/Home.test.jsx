02f427d59ea02b5a6693967d3f078856bdc4b343
Casey Cameron <mcasey.cameron@gmail.com>
2021-07-30 16:02:30 -0700

    Home Test Progress

--- src/containers/Home.test.jsx
+++ src/containers/Home.test.jsx
@@ -1,12 +1,36 @@
 import React from 'react';
 import Home from './Home';
-import { render, screen } from '@testing-library/react';
+import { render, screen, cleanup, fireEvent } from '@testing-library/react';
+import ControlForm from '../components/controls/ControlForm';
 
 describe('Home page render', () => {
+  afterEach(() => cleanup());
+
   it('Renders the home page', () => {
-    render(
-      <Home />
-    );
-    screen.getByText('RESTless');
+    // const urlValue = ({ target }) => {
+    //   this.setState({ urlValue: target.value });
+    // };
+
+    // const methodValue = (e) => {
+    //   this.setState({ method: e.target.value });
+    // };
+
+    // const bodyValue = ({ target }) => {
+    //   this.setState({ bodyValue: target.value });
+    // };
+
+    // render(<Home />);
+
+    // screen.getByText('RESTless');
+    const { queryByPlaceholderText } = render(<ControlForm
+    // urlValue={urlValue}
+    // method={methodValue}
+    // body={bodyValue}
+    />);
+
+    const searchInput = queryByPlaceholderText('URL');
+    fireEvent.change(searchInput, { target: { value: 'http://www.w.com' } });
+    expect(searchInput).toHaveValue('http://www.w.com');
+
   });
 });
